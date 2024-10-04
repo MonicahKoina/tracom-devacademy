d1f68a0d5b0b78c344dda2986347e1f8b4829561
Monicah Koina <monicahkoina1@gmail.com>
2024-10-04 16:41:26 +0300

    web update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import "./App.css";
-import Curriculum from "./Components/Curriculum";
-import Home from "./Components/Home";
-import Navbar from "./Components/Navbar";
-
-import Contact from "./Components/contact";
+import React from 'react'
+import './App.css'
+import Home from './Components/Home'
+import ContactUs from './Components/ContactUs'
+import Navbar from './Components/Navbar'
+import Curriculum from './Components/Curriculum'
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <Navbar />
-        <Home />
-        <Curriculum />
-        <Contact />
-      </div>
-    </>
-  );
+    <div className="App">
+    <Navbar />
+    <Home />
+    <Curriculum />
+    <ContactUs />
+  </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/ContactUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function ContactUs() {
+  return (
+    <div className="contact-us">
+      <h1>Contact Us</h1>
+      <div className="contact-form">
+        <form action="/" className="contact-form">
+          <ul>
+            <input type="text" placeholder="Full name" />
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Type your message" />
+          </ul>
+          <div class="form-button">
+            <button class="btn">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;
